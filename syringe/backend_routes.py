1c723b179fbbb8997ec0c03d9dfb29198f13e161
energyisalwaysconserved <ivamari.miskulin@gmail.com>
2024-02-23 17:17:37 +0000

    added docstrings

--- syringe/backend_routes.py
+++ syringe/backend_routes.py
@@ -8,24 +8,30 @@ def home():
 
 @api.route("/queue", methods=['GET'])
 def q_read_all():
+    """Gets all queues"""
     return
 
 @api.route("/queue/<int:q_id>", methods=['GET'])
 def read_q_id(q_id):
+    """Gets the queue corresponding to the q id"""
     return
 
 @api.route("/queue/<int:q_id>", methods=['POST'])
-def add_job(q_id):
+def add_step(q_id):
+    """Adds a new step to the corresponding q id"""
     return
 
 @api.route("/queue/<int:q_id>", methods=['DELETE'])
 def del_job(q_id):
+    """Deletes the queue corresponding to the q id"""
     return
 
 @api.route("/queue/<int:q_id>/<int:step_id>", methods=['DELETE'])
 def del_step(q_id, step_id):
+    """Deletes a step corresponding with step id from the queue corresponding with q id"""
     return
 
 @api.route("/queue/<int:q_id>/<int:step_id>>", methods=['UPDATE'])
 def update_step(q_id, step_id):
+    """Edits a step within a specific queue corresponding to q id. The step must not be in progress."""
     return
