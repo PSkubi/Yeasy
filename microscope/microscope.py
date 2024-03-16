73be0b02262aa1b0d451bf5d7e94cbf7a921cc52
energyisalwaysconserved <ivamari.miskulin@gmail.com>
2024-03-16 18:10:30 +0000

    implement getting images from the microscope

--- microscope/microscope.py
+++ microscope/microscope.py
@@ -1,11 +1,21 @@
 """Microscope class."""
+from pycromanager import Bridge
 
 class Microscope:
     def __init__(self):
+        # create the Micro-Managert to Pycro-Manager transfer layer
+        bridge = Bridge()
+        # get object representing micro-manager core
+        self.core = bridge.get_core()
         return
     
     def get_image(self):
-        return
+        self.core.snap_image()
+        tagged_image = self.core.get_tagged_image()
+        image_height = tagged_image.tags['Height']
+        image_width = tagged_image.tags['Width']
+        image = tagged_image.pix.reshape((image_height, image_width))
+        return image
     
     def move_stage(self, dx, dy):
         return
