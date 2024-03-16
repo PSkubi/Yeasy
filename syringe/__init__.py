9fb7c5f4ea148f0a5060361b1aeae6cbce6628f5
energyisalwaysconserved <ivamari.miskulin@gmail.com>
2024-03-16 16:46:56 +0000

    clear up

--- syringe/__init__.py
+++ syringe/__init__.py
@@ -1,11 +1,7 @@
 from flask import Flask
-
-from syringe import pages
 from syringe import backend_routes
 
 def create_app():
     app = Flask(__name__)
-
-    app.register_blueprint(pages.bp)
     app.register_blueprint(backend_routes.api, url_prefix="/api")
     return app
